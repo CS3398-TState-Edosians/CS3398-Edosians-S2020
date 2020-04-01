9f2516f7c41890a49002ac760cce70a6fd455b4f
Isaiahcollins <47799072+Isaiahcollins@users.noreply.github.com>
2020-04-01 17:20:47 -0500

    Create Server.java

---
+++ src/BattleBoats/Server.java
@@ -0,0 +1,15 @@
+import java.net. i*;
+class server
+ {
+	public static vois main { String arg[]}
+	 { 
+		try
+		n{
+		ServerSocket server = new ServerSocket{3000};
+		Socket s=server.accept;
+		
+		
+		
+		
+		System.out.println("Connected");
+}
